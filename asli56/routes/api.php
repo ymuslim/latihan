b22a3a5efd32571b83720f1498f52dbede19198d
ymuslim <yunusmuslim9@gmail.com>
2018-10-28 06:55:31 +0700

    projek

---
+++ asli56/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+	Route::post('details', 'API\UserController@details');
+});
+Route::get('logout', 'API\UserController@logout');
